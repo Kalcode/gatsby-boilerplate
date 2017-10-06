70655037f8a35956b51ac911067ad299db6105dc
David Clausen <davidclausen2051@gmail.com>
2017-10-06 11:14:16 -0500

    moved template into app for site needed

---
+++ src/components/App/index.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { BaseHelmet } from 'components/Helmet'
+import ScrollMagicContext from 'components/ScrollMagicContext'
+import 'gsap'
+import 'scrollmagic'
+if (typeof window !== 'undefined') {
+  require('../../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators')
+}
+
+const App = ({ children, location }) => (
+  <ScrollMagicContext location={location}>
+    <div>
+      <BaseHelmet location={location} />
+      {children}
+    </div>
+  </ScrollMagicContext>
+)
+
+App.propTypes = {
+  children: PropTypes.func,
+  location: PropTypes.object,
+}
+
+export default App
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,28 +1,18 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import App from 'components/App'
 import Footer from 'components/Footer'
-import { BaseHelmet } from 'components/Helmet'
 import Nav from 'components/Nav'
-import ScrollMagicContext from 'components/ScrollMagicContext'
-
-import 'gsap'
-import 'scrollmagic'
-if (typeof window !== 'undefined') {
-  require('../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators')
-}
 
 import './base.scss'
 
 const TemplateWrapper = ({ children, location }) => (
-  <ScrollMagicContext location={location}>
-    <div>
-      <BaseHelmet location={location} />
-      <Nav />
-      {children()}
-      <Footer />
-    </div>
-  </ScrollMagicContext>
+  <App location={location}>
+    <Nav />
+    {children()}
+    <Footer />
+  </App>
 )
 
 TemplateWrapper.propTypes = {
