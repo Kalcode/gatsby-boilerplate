b397214d19f9b323f8426805fd1506961a4d5a98
David Clausen <davidclausen2051@gmail.com>
2018-01-31 13:59:09 -0600

    remove example comp

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Example from 'components/ExampleComp'
 
 export default function AboutPage() {
   return (
     <main className='row'>
       <div className='small-6 columns'>
         About Us
-        <Example />
       </div>
     </main>
   )
