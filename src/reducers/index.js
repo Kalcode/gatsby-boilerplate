888b41ec79763c9a18b6a8d5eff81d2384d6f747
David Clausen <davidclausen2051@gmail.com>
2018-03-28 16:15:02 -0500

    connect prismic to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import forms from 'reducers/forms'
 import nav from 'reducers/nav'
+import prismic from 'reducers/prismic'
 
 export default combineReducers({
   forms,
   nav,
+  prismic,
 })
--- src/saga.js
+++ src/saga.js
@@ -1,6 +1,8 @@
 import { fork } from 'redux-saga/effects'
 import forms from 'reducers/forms/saga'
+import prismic from 'reducers/prismic/saga'
 
 export default function * root() {
   yield fork(forms)
+  yield fork(prismic)
 }
