c2badff795fbbd7673e0cfb4ad9be6c671904445
David Clausen <davidclausen2051@gmail.com>
2018-03-29 15:48:43 -0500

    add key events for up or down arrows

--- src/components/Slider/KeyHandler.js
+++ src/components/Slider/KeyHandler.js
@@ -15,6 +15,8 @@ export default class KeyHandler {
   onKeydown = (event) => {
     if (event.key === 'ArrowLeft') this.component.setSlide('prev')
     else if (event.key === 'ArrowRight') this.component.setSlide('next')
+    else if (event.key === 'ArrowUp') this.component.setSlide('prev')
+    else if (event.key === 'ArrowDown') this.component.setSlide('next')
     else if (event.keyCode >= 49 && event.keyCode <= 57) {
       this.component.goTo(Number(event.key) - 1)
     }
